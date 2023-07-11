c522cccbd47fc319e5d110a7274238e3652e4f00
Vitalii <warcied1@gmail.com>
2023-07-11 21:54:31 +0300

    added static index.html

--- 2. Basic servers/4. REST + Mongo/src/index.ts
+++ 2. Basic servers/4. REST + Mongo/src/index.ts
@@ -3,8 +3,11 @@ import express from 'express';
 const app = express();
 const port = 4000
 
+app.use(express.static('static'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    //res.send('Hello World!')
+    res.render('static/index.html');
 })
 
 app.listen(port, () => {
